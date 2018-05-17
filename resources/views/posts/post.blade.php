162477a44b2ff241e80b7d8dbcc90da9aca47b8f
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-05-17 12:52:18 -0400

    Added date display configuration

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -1,7 +1,9 @@
  <div class="blog-post">
 
         <h2 class="blog-post-title">{{ $post->title }}</h2>
-        <p class="blog-post-meta">{{ $post->created_at }}</p>
+        <p class="blog-post-meta">
+        
+        {{ $post->created_at->toFormattedDateString()}}</p>
 
         {{ $post->body }}
 
