e2445b64717156479163f3fe2f06e0dc8d08507b
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-06-17 09:13:40 -0400

    Added user_id

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -10,7 +10,9 @@ class CommentsController extends Controller
     //
     public function store(Post $post)
     {
+       $this->validate(request(),['body'=>'required|min:2']);
         $post->addComment(request('body'));
+        
         //add a comment to a post
 //         Comment::create([
 //             'body' => request('body'),
